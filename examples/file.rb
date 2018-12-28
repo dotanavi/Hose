43e89494df04c85b44875872d8ffd4e5b1a74e28
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-12-28 18:46:30 +0900

    Fix bug

--- examples/file.rb
+++ examples/file.rb
@@ -28,9 +28,14 @@ task 'step2', files: 'step1' do
   EOF
 end
 
-task 'final', files: 'step2' do
+task 'final', files: ['src', 'step2'] do
   bash <<-EOF
-    cat $step2 >&2
+    {
+      echo '** step2 **'
+      cat $step2
+      echo '** src **'
+      cat $src
+    } >&2
     echo DONE
   EOF
 end
